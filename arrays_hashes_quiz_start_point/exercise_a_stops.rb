b155f972610cd12c4375386bf7fa2ec9d32747c1
Graeme Henderson <graeme.189@gmail.com>
2019-06-26 18:52:12 +0100

    Complete exercises

--- arrays_hashes_quiz_start_point/exercise_a_stops.rb
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -1,12 +1,26 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 # 1. Add `"Edinburgh Waverley"` to the end of the array
+stops.push("Edinburgh Waverley")
 # 2. Add `"Glasgow Queen St"` to the start of the array
+stops.unshift("Glasgow Queen St")
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+stops.insert(4, "Polmont")
 # 4. Work out the index position of `"Linlithgow"`
+p "The index position of Linlithgow is #{stops.index("Linlithgow").to_s}"
 # 5. Remove `"Livingston"` from the array using its name
+stops.delete("Livingston")
 # 6. Delete `"Cumbernauld"` from the array by index
+stops.delete_at(2)
 # 7. How many stops there are in the array?
+p "There are #{stops.length.to_s} stops"
 # 8. How many ways can we return `"Falkirk High"` from the array?
+p stops[2] # returns 'Falkirk High'
+p stops.at(2) # returns 'Falkirk High'
+p stops[-5] # returns 'Falkirk High'
 # 9. Reverse the positions of the stops in the array
+p stops.reverse
 # 10. Print out all the stops using a for loop
+for stop in stops
+  puts stop
+end
--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -55,12 +55,46 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+p "Jonathan's Twitter handle is #{users["Jonathan"][:twitter]}"
 # 2. Get Erik's hometown
+p "Erik's hometown is #{users["Erik"][:home_town]}"
 # 3. Get the array of Erik's lottery numbers
+p "Erik's lottery numbers are:"
+p users["Erik"][:lottery_numbers]
 # 4. Get the type of Avril's pet Monty
+p "Avril's pet, Monty, is a #{users["Avril"][:pets][0][:species]}"
 # 5. Get the smallest of Erik's lottery numbers
+p "The smallest of Erik's lottery numbers is #{users["Erik"][:lottery_numbers].min.to_s}"
 # 6. Return an array of Avril's lottery numbers that are even
+even_lot_num = []
+for num in users["Avril"][:lottery_numbers]
+  if num % 2 == 0
+    even_lot_num.push(num)
+  end
+end
+p "Avril's even lottery numbers are:"
+p even_lot_num
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+p "Erik's new lottery numbers are: #{users["Erik"][:lottery_numbers].push(7)}"
 # 8. Change Erik's hometown to Edinburgh
+p "Erik's hometown is actually #{users["Erik"][:home_town] = "Edinburgh"}"
 # 9. Add a pet dog to Erik called "Fluffy"
-# 10. Add another person to the users hash
+users["Erik"][:pets].push({name: "Fluffy", species: "dog"})
+p "Erik's full list of pets: #{users["Erik"][:pets]}"
+# 10. Add another person to the users hash 
+users["Graeme"] = {
+  :twitter => "Graeme189",
+  :lottery_numbers => [1, 2, 3, 4, 5, 6],
+  :home_town => "Kirkcaldy",
+  :pets => [
+    {
+      :name => "Lola",
+      :species =>"cat"
+    },
+    {
+      :name => "Dixie",
+      :species => "cat"
+    }
+  ]
+}
+p "Here are the details for a new user called Graeme: #{users["Graeme"]}"
